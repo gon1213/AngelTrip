487a8b2eb8a82b848058e3be61cf250f29288f40
gon1213 <gon901213@gmail.com>
2017-07-09 12:13:10 -0500

    submit project

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     url(r'^schema/$', schema_view),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('angeltrip.urls')),
-    url(r'^', include('posts.urls')),
     url(r'^api-auth/', include('rest_framework.urls',)),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
